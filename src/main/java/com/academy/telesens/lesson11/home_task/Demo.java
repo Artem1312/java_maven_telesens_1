86f9da5685e677b407f06e914e26c19681a8760a
Artem1312 <48861960+Artem1312@users.noreply.github.com>
2021-01-20 22:42:14 +0200

    home task 11 d1 p3

--- src/main/java/com/academy/telesens/lesson11/home_task/Demo.java
+++ src/main/java/com/academy/telesens/lesson11/home_task/Demo.java
@@ -1,5 +1,8 @@
 package com.academy.telesens.lesson11.home_task;
 
+import com.academy.telesens.Operator.Operator;
+import com.academy.telesens.Operator.Subscriber;
+import com.academy.telesens.Person.Gender;
 import com.academy.telesens.Person.Person;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFFont;
@@ -7,44 +10,10 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Properties;
+import java.util.*;
 
 public class Demo {
     /*
-
-2) Написать Java приложение, которое наполнит файл subscribers.xlsx, случайными данными:
-	(путь к файлу взять из 'java-part.properties')
-	a) Наполнить таблицу абонентов excel(2000 строк):
-		- имена фамилии взять в соответстсвующих файлах (см. 'java-part.properties') со списком имен/фамилий (женских/мужских);
-		- возраст генерировать случайно от 5 до 90 (можно использовать Гауссово распределение для этого диапазона)
-			(диапазон брать из файла 'java-part.properties')
-
-	b)  Телефонные номера для каждого оператора со следующими префиксами:
-		- Life номера с префиксами: 38063*******, 38093*******, 38073*******
-		- Kievstar номера с префиксами: 38097*******, 38067*******, 38098*******
-		- Vodafone номера с префиксами: 38050*******, 38066*******, 38095*******
-
-	Результат subscribers.xlsx должен выглядеть так:
-		   1 | Васильев  | Иван | м | 23 | 380630025465 | Life
-		   2 | Петрова   | Катя | ж | 34 | 380670058694 | Kievstar
-		...
-		2000 | Борисов   | Коля | м | 48 | 380500025465 | Vodafone
-		Всего 200 случайных строк
-
-	с) Реализовать класс Subscriber с приватными полями:
-		- private Long id;
-		- private String firstName;
-		- private String lastName;
-		- private Gender gender; // создать перечисление Gender для мужского и женского пола
-			- 'MALE'
-			- 'FEMALE'
-		- private int age;
-		- private String phoneNumber;
-		- private Operator operator;
-				private Long id;
-				private String name;
 
 	d) Прочитать subscribers.xlsx в коллекцию Map<Long, Subscriber> и сохранить в текстовый файл: subscribers.txt
 		(путь к файлу взять из 'java-part.properties')
@@ -123,74 +92,124 @@ public class Demo {
 			информация о само абоненте (взять через toString())
      */
     public static void main(String[] args) {
-        /*
-        2) Написать Java приложение, которое наполнит файл subscribers.xlsx, случайными данными:
-
-	a) Наполнить таблицу абонентов excel(2000 строк):
-		- имена фамилии взять в соответстсвующих файлах (см. 'java-part.properties') со списком имен/фамилий (женских/мужских);
-		- возраст генерировать случайно от 5 до 90 (можно использовать Гауссово распределение для этого диапазона)
-			(диапазон брать из файла 'java-part.properties')
-
-	b)  Телефонные номера для каждого оператора со следующими префиксами:
-		- Life номера с префиксами: 38063*******, 38093*******, 38073*******
-		- Kievstar номера с префиксами: 38097*******, 38067*******, 38098*******
-		- Vodafone номера с префиксами: 38050*******, 38066*******, 38095*******
-
-	Результат subscribers.xlsx должен выглядеть так:
-		   1 | Васильев  | Иван | м | 23 | 380630025465 | Life
-		   2 | Петрова   | Катя | ж | 34 | 380670058694 | Kievstar
-		...
-		2000 | Борисов   | Коля | м | 48 | 380500025465 | Vodafone
-		Всего 200 случайных строк
-         */
         Properties properties = new Properties();
         File file = new File("src/main/resources/java-part.properties");
         String path = "";
+        String mans_name = "";
+        String mans_surname = "";
+        String women_name = "";
+        String women_surname = "";
+
         try (FileInputStream fis = new FileInputStream(file)) {
             properties.load(fis);
             path = properties.getProperty("subscriber.exc");
+            mans_name = properties.getProperty("male.firstnames");
+            mans_surname = properties.getProperty("male.lastnames");
+            women_name = properties.getProperty("female.firstnames");
+            women_surname = properties.getProperty("female.lastnames");
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        ArrayList<String> name_man = new ArrayList<>();
+        ArrayList<String> surname_man = new ArrayList<>();
+        ArrayList<String> name_women = new ArrayList<>();
+        ArrayList<String> surname_women = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(mans_name))){
+            String line;
+            while ((line = br.readLine())!=null)
+                name_man.add(line);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader br2 = new BufferedReader(new FileReader(mans_surname))){
+            String line;
+            while ((line = br2.readLine())!=null)
+                surname_man.add(line);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader br3 = new BufferedReader(new FileReader(women_name))){
+            String line;
+            while ((line = br3.readLine())!=null)
+                name_women.add(line);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader br4 = new BufferedReader(new FileReader(women_surname))){
+            String line;
+            while ((line = br4.readLine())!=null)
+                surname_women.add(line);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("Subscribers");
-//        Object[][] date = {
-//                {1,"Васильев", "Иван", "m", 23, "380630025465", "Life"},
-//                {1,"Васильев", "Иван", "m", 23, "380630025465", "Life"}
-//        };
 
-        Object[][] user = {
-                {1,"Васильев", "Иван", "m", 23, "380630025465", "Life"},
-                {1,"Васильев", "Иван", "m", 23, "380630025465", "Life"}
-        };
+        Random random = new Random();
+        Gender[] gender = new Gender[2000];
+        int[] age = new int[2000];
+        String[] operator = new String[2000];
+        String[] phoneNumber = new String[2000];
 
-        //ArrayList<Object> date = new ArrayList<>(Arrays.asList(user));
-        ArrayList<Object> date = new ArrayList<>(Arrays.asList(new ArrayList<>(Arrays.asList(user))));
+        for (int i = 0; i < age.length ; i++) {
+            age[i] = random.nextInt(90+1-5)+5;
+        }
 
-        System.out.println(Arrays.deepToString(date.toArray()));
-        System.out.println(Arrays.deepToString((Object[]) date.get(0)) );
+        for (int g = 0; g < gender.length; g++) {
+            if(random.nextInt(2) == 0){
+                gender[g] = Gender.MALE;
+            } else { gender[g] = Gender.FEMALE;}
+        }
 
+        for (int i = 0; i < operator.length; i++) {
+            if (random.nextInt(3) == 0){
+                operator[i] = String.valueOf(MobileOperator.LIFE);
+                if(random.nextInt(3) == 0) {
+                    phoneNumber[i] = MobileOperator.LIFE.getFirstIndex()+ (random.nextInt(1000000)+999999);
+                } else if (random.nextInt(3) == 1) {
+                    phoneNumber[i] = MobileOperator.LIFE.getSecondIndex()+ (random.nextInt(1000000)+999999);
+                } else {
+                    phoneNumber[i] = MobileOperator.LIFE.getThirdIndex()+ (random.nextInt(1000000)+999999);
+                }
+            } else if (random.nextInt(3)==1){
+                operator[i] = String.valueOf(MobileOperator.KIEVSTAR);
+                if(random.nextInt(3) == 0) {
+                    phoneNumber[i] = MobileOperator.KIEVSTAR.getFirstIndex()+ (random.nextInt(1000000)+999999);
+                } else if (random.nextInt(3) == 1) {
+                    phoneNumber[i] = MobileOperator.KIEVSTAR.getSecondIndex()+ (random.nextInt(1000000)+999999);
+                } else {
+                    phoneNumber[i] = MobileOperator.KIEVSTAR.getThirdIndex()+ (random.nextInt(1000000)+999999);
+                }
+            } else  {
+                operator[i] = String.valueOf(MobileOperator.VODAFONE);
+                if(random.nextInt(3) == 0) {
+                    phoneNumber[i] = MobileOperator.VODAFONE.getFirstIndex()+ (random.nextInt(1000000)+999999);
+                } else if (random.nextInt(3) == 1) {
+                    phoneNumber[i] = MobileOperator.VODAFONE.getSecondIndex()+ (random.nextInt(1000000)+999999);
+                } else {
+                    phoneNumber[i] = MobileOperator.VODAFONE.getThirdIndex()+ (random.nextInt(1000000)+999999);
+                }
+            }
+        }
 
-        //System.out.println( (Arrays.deepToString(((Object[]) date.get(0)))  ) );
-        //Object[] test =  {date.get(1)};
-        //System.out.println(Arrays.toString(test);
-
-//        for (int i = 0; i <test.length ; i++) {
-//            System.out.println(i);
-//        }
-
-        //String k = (String) test[2];
-        //System.out.println(k);
-        //System.out.println(Arrays.toString((Object[]) test[0]));
-
-        //System.out.println(date.stream());
-
-
-        for (int r = 0; r < 1; r++) {
+        for (int r = 0; r < 2000; r++) {
             Row row = sheet.createRow(r);
             Cell cellIndex = row.createCell(0);
             Cell cellSecondName = row.createCell(1);
@@ -199,24 +218,29 @@ public class Demo {
             Cell cellAge = row.createCell(4);
             Cell cellPhoneNumber = row.createCell(5);
             Cell cellOperator = row.createCell(6);
-            
-//            cellIndex.setCellValue((int)date[r][0]);
-//            cellIndex.setCellValue((date.get()[r][0]));
-//
-//            cellSecondName.setCellValue((String) date[r][1]);
-//            cellFirstName.setCellValue((String)date[r][2]);
-//            cellGender.setCellValue((String)date[r][3]);
-//            cellAge.setCellValue((int)date[r][4]);
-//            cellPhoneNumber.setCellValue((String)date[r][5]);
-//            cellOperator.setCellValue((String)date[r][6]);
 
+            cellIndex.setCellValue(r+1);
+            cellAge.setCellValue(age[r]);
+            cellOperator.setCellValue((String)operator[r]);
+            cellPhoneNumber.setCellValue((String)phoneNumber[r]);
 
-
+            if(gender[r] == Gender.MALE){
+                cellGender.setCellValue( (String) "M");
+                cellSecondName.setCellValue((String) name_man.get(random.nextInt(name_man.size()-1)));
+                cellFirstName.setCellValue((String) surname_man.get(random.nextInt(surname_man.size()-1)));
+            } else {
+                cellGender.setCellValue((String)"F");
+                cellSecondName.setCellValue((String) name_women.get( random.nextInt(name_women.size()-1)));
+                cellFirstName.setCellValue((String) surname_women.get(random.nextInt(surname_women.size()-1)));
+            }
         }
-        try (FileOutputStream out = new FileOutputStream(new File(path))) { 
+
+        try (FileOutputStream out = new FileOutputStream(new File(path))) {
             workbook.write(out);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
+
