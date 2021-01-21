8bc3dbcb77f1e162459f3997d4936ac60f788626
Artem1312 <48861960+Artem1312@users.noreply.github.com>
2021-01-21 19:54:12 +0200

    lesson 12 exel

---
+++ src/main/java/com/academy/telesens/lesson12/Demo2_read_rows.java
@@ -0,0 +1,35 @@
+package com.academy.telesens.lesson12;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Demo2_read_rows {
+    public static void main(String[] args) {
+        String path = "C:\\java-maven-1\\src\\main\\java\\com\\academy\\telesens\\lesson12\\demo.xlsx";
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            Workbook workbook = new XSSFWorkbook(fis);
+            Sheet sheet =  workbook.getSheetAt(0);
+
+            for (int i = 0; i <=sheet.getLastRowNum(); i++) {
+                Row row = sheet.getRow(i);
+
+                Cell cell1 = row.getCell(0);
+                Cell cell2 = row.getCell(1);
+                Cell cell3 = row.getCell(2);
+                System.out.println(cell1.getStringCellValue()+" "+ cell2.getStringCellValue()+ " "+ cell3.getNumericCellValue());
+            }
+
+            workbook.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
